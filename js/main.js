ed4acdd72f275af6dbd34c75564ca3dfbd7c532b
teoto4 <teoto@ukr.net>
2024-10-30 23:17:12 +0200

    Обновление времени

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
 //DOM Elements
 const calendar = document.querySelector('#calendar');
 const dates = document.querySelector('#dates')
+const all_dates = document.querySelectorAll('td');
+const event_section = document.querySelector('.event_section');
+const time = document.getElementById('time');
 
-
+console.log(event_section);
 //Start
 // let year = 2024;
 // let month = 0;
 
-const now = new Date(0);
-console.log(now);
+let timer_interval = setInterval(()=>{
+    const now = new Date();
+    console.log('hello');
+    time.textContent = now.getHours() +":" + now.getMinutes() + ":" + now.getSeconds();
+}, 1000);
+
+
+
+for(let i = 0; i < all_dates.length; i++){
+    all_dates[i].addEventListener(('click'), (e)=>{
+        console.log('working');
+
+        if(event_section.style.display === 'flex'){
+            event_section.style.display = 'none';
+        } else{
+            event_section.style.display = 'flex'
+        }
+    });
+}
