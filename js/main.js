bf9db9c5c4663b0e39a5feb496a0a90f44d1b447
teoto4 <teoto@ukr.net>
2024-10-30 22:42:37 +0200

    Подготовка для изменяющихся элементов + перевёл псевдокласс в отдельный класс для добавления в js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//DOM Elements
+const calendar = document.querySelector('#calendar');
+const dates = document.querySelector('#dates')
+
+
+//Start
+// let year = 2024;
+// let month = 0;
+
+const now = new Date(0);
+console.log(now);
